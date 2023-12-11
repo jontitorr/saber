729e9ac8ac0a0f7659a5a12e77d2aa9b291a1741
Xminent <mail@xminent.com>
2023-12-11 12:53:21 -0600

    refactor: :necktie: exit early if DISCORD_TOKEN is not provided

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
+#include <iostream>
 #include <saber/saber.hpp>
 
 int main() {
-	auto saber = saber::Saber{std::getenv("DISCORD_TOKEN")};
+	const auto* token = std::getenv("DISCORD_TOKEN");
+
+	if (token == nullptr) {
+		std::cerr << "Missing DISCORD_TOKEN environment variable\n";
+		return 1;
+	}
+
+	auto saber = saber::Saber{token};
 
 	saber.run();
 }
