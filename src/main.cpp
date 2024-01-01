cf3945de5754929abbeec792859e725c409ff140
Xminent <mail@xminent.com>
2023-12-31 23:39:58 -0600

    chore: :recycle: update missing token error message

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,9 @@ async_main(const boost::asio::yield_context& yield) {
 	BOOST_OUTCOME_TRY(auto config, saber::Config::from_file("config.json"));
 
 	if (config.token.empty()) {
-		fmt::println("Missing DISCORD_TOKEN environment variable");
+		fmt::println(
+			"Missing `token` in config.json. Please include or use the "
+			"`SABER_TOKEN` environment variable.");
 		return boost::system::errc::invalid_argument;
 	}
 
