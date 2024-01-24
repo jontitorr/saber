1d1a099cf6b352ca55097c83f382c22908dc6e57
Xminent <mail@xminent.com>
2024-01-23 21:53:30 -0600

    feat: :sparkles: add sigint handler

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,28 @@
-#include <ekizu/async_main.hpp>
+#include <boost/asio/detached.hpp>
+#include <boost/asio/signal_set.hpp>
 #include <saber/saber.hpp>
 
-async_main(const boost::asio::yield_context& yield) {
-	SABER_TRY(auto config, saber::Config::from_file("config.json"));
+int main() {
+	boost::asio::io_context ctx;
+	auto config = saber::Config::from_file("config.json");
 
-	if (config.token.empty()) {
+	if (!config || config.value().token.empty()) {
 		fmt::println(
 			"Missing `token` in config.json. Please include or use the "
 			"`SABER_TOKEN` environment variable.");
 		return boost::system::errc::invalid_argument;
 	}
 
-	auto saber = saber::Saber{config};
-	saber.run(yield);
+	auto saber = saber::Saber{config.value()};
 
-	return ekizu::outcome::success();
+	spawn(
+		ctx, [&saber](auto y) { saber.run(y); }, boost::asio::detached);
+
+	// TODO: Implement graceful shutdown
+	boost::asio::signal_set signals(ctx, SIGINT, SIGTERM);
+	signals.async_wait([&ctx](const boost::system::error_code& ec, int) {
+		if (!ec) { ctx.stop(); }
+	});
+
+	ctx.run();
 }
